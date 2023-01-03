cb01a81cb773a268760bcb353350f87c82ace312
Aravind Marri <aravindmarri99@gmail.com>
2023-01-03 23:44:22 +0530

    created component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatedComponentComponent } from './created-component/created-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatedComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/created-component/created-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-created-component',
+  templateUrl: './created-component.component.html',
+  styleUrls: ['./created-component.component.scss']
+})
+export class CreatedComponentComponent {
+
+}
